1d92cab670b940a5abb68d124c8365af040a64f9
Shuaib Sajid <shusajid@gmail.com>
2018-05-29 09:17:26 -0500

    file creation completed, exercise done, unlinked to html

---
+++ main.js
@@ -0,0 +1,9 @@
+// Open VS Code and open a new Quokka Javascript file.
+// 2. Create an array that holds at least five items. Each item should be a string that describes your experiance at NSS so far.
+// 3. Iterate over the array and console log each item in the array.
+
+const nssExperience=["fun", "quick", "minor fustrations","major victories","forward-facing"];
+
+for(let i = 0; i< nssExperience.length;i++){
+    console.log(nssExperience[i]);
+}
